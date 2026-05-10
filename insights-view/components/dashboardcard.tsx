a103f868b2701d1daee06088da43a2136c36e89e
atomnoid <aayushs2580@gmail.com>
2026-05-10 22:18:05 +0530

    added dashboard component

---
+++ insights-view/components/dashboardcard.tsx
@@ -0,0 +1,23 @@
+type DashboardCardProps = {
+  title: string;
+  value: string;
+};
+
+export default function DashboardCard({
+  title,
+  value,
+}: DashboardCardProps) {
+  return (
+    <div className="bg-zinc-900 p-6 rounded-2xl border border-zinc-800 w-[300px]">
+
+      <h2 className="text-2xl font-semibold mb-2">
+        {title}
+      </h2>
+
+      <p className="text-5xl font-bold">
+        {value}
+      </p>
+
+    </div>
+  );
+}
