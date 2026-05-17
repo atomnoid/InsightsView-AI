abf6a742f77251447f33f595733e7db872f98d49
atomnoid <aayushs2580@gmail.com>
2026-05-17 22:31:49 +0530

    added comments

--- insights-view/components/dashboardcard.tsx
+++ insights-view/components/dashboardcard.tsx
@@ -2,21 +2,16 @@ type DashboardCardProps = {
   title: string;
   value: string;
 };
+
+// A simple card component that shows a stat name and its value.
 export default function DashboardCard({
   title,
   value,
 }: DashboardCardProps) {
   return (
     <div className="bg-zinc-900 p-6 rounded-2xl border border-zinc-800 w-[300px]">
-
-      <h2 className="text-2xl font-semibold mb-2">
-        {title}
-      </h2>
-
-      <p className="text-5xl font-bold">
-        {value}
-      </p>
-
+      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+      <p className="text-5xl font-bold">{value}</p>
     </div>
   );
 }
